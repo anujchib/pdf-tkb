822ea359fee1dae81cc734742d91e7e4db7c75d1
anujchib <anujchibofficial0003@gmail.com>
2025-04-19 14:21:00 +0530

    client url

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ const app = express();
 
 // Configure CORS
 app.use(cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:3000", "http://localhost:5500"],
+    origin: ["http://127.0.0.1:5500", "http://localhost:3000", "http://localhost:5500",
+
+       "https://pdf-tkf.vercel.app"
+
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
